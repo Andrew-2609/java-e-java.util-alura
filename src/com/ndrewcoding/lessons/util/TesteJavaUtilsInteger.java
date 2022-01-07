0553810997df5ae071a659c3c94ee45433842efb
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-07 10:03:30 -0300

    Feat: add basic implementation of TesteJavaUtilsInteger

--- src/com/ndrewcoding/lessons/util/TesteJavaUtilsInteger.java
+++ src/com/ndrewcoding/lessons/util/TesteJavaUtilsInteger.java
@@ -1,4 +1,29 @@
 package com.ndrewcoding.lessons.util;
 
+@SuppressWarnings({"UnnecessaryBoxing", "WrapperTypeMayBePrimitive"})
 public class TesteJavaUtilsInteger {
+    public static void main(String[] args) {
+
+        System.out.println("Número de bytes de um Integer: " + Integer.BYTES);
+
+        int idade = 19;
+        Integer idadeRef = Integer.valueOf(idade); // autoboxing
+        double valor = idadeRef.doubleValue(); // unboxing
+
+        System.out.println("Valor depois de Autoboxing e Unboxing: " + valor);
+
+        String s;
+        try {
+            s = args[0];
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            s = "42";
+        }
+
+        Integer integerRef = Integer.valueOf(s);
+        int primitivo = Integer.parseInt(s);
+
+        System.out.println("Parâmetro informado convertido para Integer: " + integerRef);
+        System.out.println("Parâmetro informado convertido para int: " + primitivo);
+
+    }
 }
