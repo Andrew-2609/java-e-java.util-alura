bcfbcc604afa98acce9f1e5c133d469d1d83715a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-07 07:17:09 -0300

    Feat: add basic implementation of GuardadorDeContas.java

--- src/com/ndrewcoding/models/GuardadorDeContas.java
+++ src/com/ndrewcoding/models/GuardadorDeContas.java
@@ -1,2 +1,20 @@
-package com.ndrewcoding.models;public class GuardadorDeContas {
+package com.ndrewcoding.models;
+
+import models.Conta;
+
+public class GuardadorDeContas {
+
+    private Conta[] referencias;
+    private int posicaoLivre;
+
+    public GuardadorDeContas() {
+        this.referencias = new Conta[10];
+        this.posicaoLivre = 0;
+    }
+
+    public void guardar(Conta conta) {
+        this.referencias[this.posicaoLivre] = conta;
+        this.posicaoLivre++;
+    }
+
 }
