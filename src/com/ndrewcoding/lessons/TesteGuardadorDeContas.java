c1146062607da22e21acb8eb719e6ac8131040f5
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-07 07:17:31 -0300

    Feat: add basic implementation of TesteGuardadorDeContas

--- src/com/ndrewcoding/lessons/TesteGuardadorDeContas.java
+++ src/com/ndrewcoding/lessons/TesteGuardadorDeContas.java
@@ -1,2 +1,22 @@
-package com.ndrewcoding.lessons;public class TesteGuardadorDeContas {
+package com.ndrewcoding.lessons;
+
+import com.ndrewcoding.models.GuardadorDeContas;
+import models.ContaCorrente;
+import models.ContaPoupanca;
+
+public class TesteGuardadorDeContas {
+    public static void main(String[] args) {
+
+        GuardadorDeContas guardadorDeContas = new GuardadorDeContas();
+
+        ContaCorrente contaCorrente = new ContaCorrente(123, 321);
+        contaCorrente.depositar(4000.0);
+
+        ContaPoupanca contaPoupanca = new ContaPoupanca(456, 654);
+        contaPoupanca.depositar(6000.0);
+
+        guardadorDeContas.guardar(contaCorrente);
+        guardadorDeContas.guardar(contaPoupanca);
+
+    }
 }
