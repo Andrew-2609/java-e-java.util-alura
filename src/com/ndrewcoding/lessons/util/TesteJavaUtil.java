ef5480d73f82eedb0d4fef188f14d75621cb0a2a
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-07 07:34:31 -0300

    Feat: add basic implementation of TesteJavaUtil

--- src/com/ndrewcoding/lessons/util/TesteJavaUtil.java
+++ src/com/ndrewcoding/lessons/util/TesteJavaUtil.java
@@ -1,4 +1,42 @@
 package com.ndrewcoding.lessons.util;
 
+import models.Conta;
+import models.ContaCorrente;
+import models.ContaPoupanca;
+
+import java.util.ArrayList;
+
 public class TesteJavaUtil {
+    public static void main(String[] args) {
+
+        ArrayList lista = new ArrayList();
+
+        ContaCorrente contaCorrente = new ContaCorrente(123, 321);
+        contaCorrente.depositar(1000.0);
+
+        lista.add(contaCorrente);
+
+        ContaPoupanca contaPoupanca = new ContaPoupanca(456, 654);
+        contaPoupanca.depositar(2000.0);
+
+        lista.add(contaPoupanca);
+
+        System.out.println("Tamanho atual da lista: " + lista.size());
+
+        Conta ref = (Conta) lista.get(0);
+        System.out.println(ref);
+
+        lista.remove(1);
+
+        System.out.println("Tamanho atual da lista: " + lista.size());
+
+        lista.add(contaPoupanca);
+
+        System.out.println("-----");
+
+        for (Object obj : lista) {
+            System.out.println(obj);
+        }
+
+    }
 }
