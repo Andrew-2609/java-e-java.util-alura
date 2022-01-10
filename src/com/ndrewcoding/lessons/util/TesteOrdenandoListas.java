0a78181016a0da0d7c38221841eb687b25383bc0
Andrew Monteiro <andrew.agoravai@gmail.com>
2022-01-09 21:12:36 -0300

    Feat: implement comparation with 'titular' attribute of Conta

--- src/com/ndrewcoding/lessons/util/TesteOrdenandoListas.java
+++ src/com/ndrewcoding/lessons/util/TesteOrdenandoListas.java
@@ -1,5 +1,6 @@
 package com.ndrewcoding.lessons.util;
 
+import models.Cliente;
 import models.Conta;
 import models.ContaCorrente;
 import models.ContaPoupanca;
@@ -11,16 +12,30 @@ import java.util.List;
 public class TesteOrdenandoListas {
     public static void main(String[] args) {
 
+        Cliente primeiroCliente = new Cliente();
+        Cliente segundoCliente = new Cliente();
+        Cliente terceiroCliente = new Cliente();
+        Cliente quartoCliente = new Cliente();
+
+        primeiroCliente.setNome("Andrew Monteiro");
+        segundoCliente.setNome("Pedro Lucas");
+        terceiroCliente.setNome("Marco Isaías");
+        quartoCliente.setNome("Marco Ian");
+
         Conta cc1 = new ContaCorrente(24, 33);
+        cc1.setTitular(primeiroCliente);
         cc1.depositar(333.0);
 
         Conta cc2 = new ContaPoupanca(23, 44);
+        cc2.setTitular(segundoCliente);
         cc2.depositar(444.0);
 
         Conta cc3 = new ContaCorrente(22, 11);
+        cc3.setTitular(terceiroCliente);
         cc3.depositar(111.0);
 
         Conta cc4 = new ContaPoupanca(22, 22);
+        cc4.setTitular(quartoCliente);
         cc4.depositar(222.0);
 
         List<Conta> listaDeContas = new ArrayList<>();
@@ -33,12 +48,20 @@ public class TesteOrdenandoListas {
             System.out.println(conta);
         }
 
-        System.out.println("----- Após ordenação das contas por *agência* -----");
+        System.out.println("\n----- Após ordenação das contas por *agência* -----\n");
 
         listaDeContas.sort(new ComparadorDeContasPorAgencia());
 
         for (Conta conta : listaDeContas) {
             System.out.println(conta);
+        }
+
+        System.out.println("\n----- Após ordenação das contas por *titular* -----\n");
+
+        listaDeContas.sort(new ComparadorDeContasPorTitular());
+
+        for (Conta conta : listaDeContas) {
+            System.out.println(conta + "; Titular: " + conta.getTitular().getNome());
         }
 
     }
